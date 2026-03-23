ee2105d43621200588e75faca5f26916a382b68f
ozgeyilmazh <ucankacanmavituy@gmail.com>
2026-03-23 08:20:05 +0300

    GREEN test for scrapeactivity workflow

--- scrape-scheduler/workflow_test.go
+++ scrape-scheduler/workflow_test.go
@@ -63,7 +63,17 @@ type QuotesScrapeSchedulerWorkflow struct {
 	ScrapeActivity  func(ctx context.Context, start int, end int) error
 }
 
-func (w *QuotesScrapeSchedulerWorkflow) Execute(ctx context.Context, start int, end int) (QuotesScrapeSchedulerResult, error) {
+func (w *QuotesScrapeSchedulerWorkflow) Execute(ctx workflow.Context, start int, end int) (QuotesScrapeSchedulerResult, error) {
+	w.Logger.Info("Executing quotes scrape scheduler workflow", "start", start, "end", end)
+
+	ctx = workflow.WithActivityOptions(ctx, w.ActivityOptions)
+	err := workflow.ExecuteActivity(ctx, w.ScrapeActivity, start, end).Get(ctx, nil)
+	if err != nil {
+		w.Logger.Error("Failed to execute quotes scrape activity", "error", err)
+		return QuotesScrapeSchedulerResult{Status: QuotesScraperStatusFailed}, err
+	}
+
+	w.Logger.Info("Quotes scrape scheduler workflow executed successfully", "start", start, "end", end)
 	return QuotesScrapeSchedulerResult{Status: QuotesScraperStatusScheduled}, nil
 }
 
