1aa73f45f11a196994784200a2c585c6868eb7fd
ozgeyilmazh <ucankacanmavituy@gmail.com>
2026-03-23 08:51:52 +0300

    Green test for Add scraping trigger functionality to QuotesScraperGatewayClient with error handling for HTTP requests

--- scrape-scheduler/scraper_gateway_client.go
+++ scrape-scheduler/scraper_gateway_client.go
@@ -2,6 +2,7 @@ package scrape_scheduler
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
 	"net/http"
 )
@@ -22,5 +23,22 @@ func NewQuotesScraperGatewayClient(host string, logger *slog.Logger) *QuotesScra
 
 func (c *QuotesScraperGatewayClient) TriggerScraping(ctx context.Context, start int, end int) error {
 	c.logger.Info("Triggering scraping", "start", start, "end", end)
+	url := fmt.Sprintf("%s/scrape/quotes/start/%d/end/%d", c.host, start, end)
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, url, nil)
+	if err != nil {
+		return err
+	}
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if resp.Body != nil {
+			resp.Body.Close()
+		}
+	}()
+	if resp.StatusCode != http.StatusAccepted {
+		return fmt.Errorf("failed to trigger scraping: %s", resp.Status)
+	}
 	return nil
 }
